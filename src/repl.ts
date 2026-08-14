c8a6cba82d196b0c29d951af8e76afb9a9e3fb98
kasjank <kowalczykkasjan@gmail.com>
2026-08-14 22:35:42 +0200

    feat: repl with exit and help commands

--- src/repl.ts
+++ src/repl.ts
@@ -1,3 +1,56 @@
+import { createInterface } from "readline";
+import { commandExit } from "./command_exit.js";
+import { commandHelp } from "./command_help.js";
+import { CLICommand } from "./command.js";
+
+
+export function getCommands(): Record<string, CLICommand> {
+    return {
+        exit: {
+            name: "exit",
+            description: "Exit the Pokedex",
+            callback: commandExit,
+        },
+        help: {
+            name: "help",
+            description: "Displays a help message",
+            callback: commandHelp,
+        }
+    };
+}
+
+export function startREPL() {
+    const rl = createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        prompt: "Pokedex > ",
+    });
+    rl.prompt();
+    rl.on('line', (input) => {
+        const words = cleanInput(input);
+        if (words.length == 0) {
+            rl.prompt();
+            return;
+        }
+        const commandName = words[0];
+        const commandList = getCommands()
+        const cmd = commandList[commandName]
+        if (!cmd) {
+            console.log(`Unknown command: "${commandName}"`);
+            rl.prompt();
+            return
+        }
+
+        try {
+            cmd.callback(commandList);
+        } catch (e) {
+            console.log(e);
+        }
+        rl.prompt();
+    });
+}
+
 export function cleanInput(input: string): string[] {
     return input.toLowerCase().trim().split(" ").filter((word) => word !== "");
 }
+
